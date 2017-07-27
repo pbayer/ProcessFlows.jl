5f991dc4bd0bb4d70c37a46a1daa41f5ca238891
pbayer <Paul.Bayer@gleichsam.de>
2017-07-27 16:45:05 +0200

    logging further simplified

--- src/simlog.jl
+++ src/simlog.jl
@@ -8,19 +8,21 @@ create, initialize and return a new SimLog containing
 access to variables and to measurements taken during simulation
 """
 mutable struct SimLog
-  vars::Any
-  measurements::Any
+  vars
+  redict
+  prefix
+  measurements
 end
 
 
 """
-    logvar(name::AbstractString, value::Any)
+    logvar(name::AbstractString, value::Union{AbstractString,Number})
 
 create, initialize and return a new logging variable
 """
 mutable struct logvar
   name::AbstractString
-  value::Any
+  value::Union{AbstractString,Number}
 end
 
 
@@ -30,16 +32,16 @@ end
 create and return a new empty `SimLog`
 """
 function newlog()
-  SimLog(Dict(), Dict())
+  SimLog(Dict(), Dict(), Dict(), Dict())
 end
 
 """
-    add2log(simlog, vars...)
+    logvar2log(simlog, vars...)
 
-add several `logvar` variables to a `SimLog`
+add one or several `logvar` variables to a `SimLog`
 and initialize the `measurements` Dict
 """
-function add2log(simlog, vars...)
+function logvar2log(simlog, vars...)
   if !haskey(simlog.measurements, " time")
     simlog.measurements[" time"] = []
   end
@@ -50,6 +52,37 @@ function add2log(simlog, vars...)
 end
 
 """
+    dict2log(simlog, dicts...; prefix::AbstractString="", redict=Dict())
+
+add the elements of one or several `Dicts` to a `Simlog`
+and initialise the `measurements` Dict
+
+# Arguments
+- `simlog`: the log, the dict elements get added to
+- `dict`: the dict, whose elements get added
+- ''
+- `prefix`: a string which gets prefixed to all logged keys
+- `redict`: a dict containing renamings for dictionary keys,
+  e.g. Dict(1=>"A", 2=>"B")
+
+"""
+function dict2log(simlog, dict; prefix::AbstractString="", redict=Dict())
+  name = string(hash(dict))
+  if !haskey(simlog.measurements, " time")
+    simlog.measurements[" time"] = []
+  end
+  simlog.vars[name] = dict
+  simlog.prefix[name] = prefix
+  simlog.redict[name] = redict
+  for k in keys(dict)
+    n = haskey(redict, k) ? redict[k] : k
+    n = prefix*string(n)
+    simlog.measurements[n] = []
+  end
+end
+
+
+"""
     lognow(sim::Simulation, simlog)
 
 take actual "measurements" of all registered `logvar` variables
@@ -57,8 +90,18 @@ and add them to the `simlog.measurements` dictionary entries.
 """
 function lognow(sim::Simulation, simlog)
   push!(simlog.measurements[" time"], now(sim))
-  for v in keys(simlog.vars)
-    push!(simlog.measurements[v], simlog.vars[v].value)
+  for v in keys(simlog.vars)                          # check all simlog vars
+    if typeof(simlog.vars[v]) == logvar                       # is it a logvar?
+      push!(simlog.measurements[v], simlog.vars[v].value)
+    elseif typeof(try haskey(simlog.vars[v], 0) end) == Bool  # is it a dict?
+      for k in keys(simlog.vars[v])
+        n = haskey(simlog.redict[v], k) ? simlog.redict[v][k] : k
+        n = simlog.prefix[v]*string(n)
+        push!(simlog.measurements[n], simlog.vars[v][k])
+      end
+    else
+      return
+    end
   end
 end
 
@@ -102,7 +145,6 @@ end
 transform the `SimLog` to a DataFrame and return it.
 """
 function log2df(simlog)
-
   df = DataFrame(vals(simlog.measurements))
   rename!(df, Dict(Symbol(" time")=>:time))
 end
