6d1e756de40fc5615db05d8a376821c008d49743
pbayer <Paul.Bayer@gleichsam.de>
2017-09-07 13:29:29 +0200

    product flow graph implemented

--- TODO.md
+++ TODO.md
@@ -15,7 +15,7 @@ it fit for general use in simulation of production and project environments:
 - [ ] (0.1) create transport jobs only if the input queue of the target is not full
 - [ ] (0.2) implement priority scheduling
 - [ ] (0.2) implement scheduling strategies (CONWIP, DBR and the like)
-- [ ] (0.2) implement jobs, in which the sequence doesn't matter (healthcare)
+- [ ] (0.2) implement jobs, in which the sequence doesn't matter (eg. healthcare)
 
 ### Documentation
 
@@ -23,13 +23,15 @@ it fit for general use in simulation of production and project environments:
 - [ ] (0.1) Outline
 - [ ] (0.1) Examples
 
-### Stats and visualization: `viz.jl`
+### Tables, stats and visualization: `eval.jl`, `viz.jl`, `graphs.jl`
 
 - [ ] (0.1) queue lengths
+- [ ] (0.1) waiting times
 - [x] (0.1) lead times
 - [x] (0.1) workload
-- [x] (0.1) workflow 
-- [ ] (0.2) workflow (network graph)
+- [x] (0.1) workflow
+- [x] (0.2) order flow (network graph)
+- [x] (0.2) product flow (network graph)
 - [ ] (0.2) resource constraints
 
 ### Convenience
--- src/graphs.jl
+++ src/graphs.jl
@@ -8,44 +8,65 @@
 # --------------------------------------------
 
 """
-    ordergraph(ord::Orders)
+    ordergraph(ord::Orders, os::Array{String,1})
 
-return a Digraph built from a PFlow Orders dict
+build a Digraph from a PFlow Orders dict
+
+# Parameters
+- `ord::Orders`: Orders dict
+- `os::Array{String,1}`: names of selected orders, if empty all orders are taken
+
+# Returns
+(G, edgenames, weights) where
+- `G::SimpleDiGraph{Int64}`: graph structure
+- `edgenames::Array{String,1}`: workunit names for vertices
+- `weights::Array{Int64,1}`: number of orders using the edges
 """
-function ordergraph(ord::Orders)
-    ws = String[]              # create access to work units
-    for o ∈ keys(ord)
+function ordergraph(ord::Orders, os::Array{String,1})
+    if length(os) == 0
+        os = keys(ord)
+    end
+
+    wus = String[]              # create access to work units
+    for o ∈ os
         for j ∈ ord[o]
-            append!(ws, j.wus)
+            append!(wus, j.wus)
         end
     end
-    ws = collect(Set(ws))
-    sort!(ws)
-    unshift!(ws, "IN")
-    push!(ws, "OUT")
+    wus = collect(Set(wus))
+    sort!(wus)
+    unshift!(wus, "IN")
+    push!(wus, "OUT")
     wu = Dict()
-    for (i, v) ∈ enumerate(ws)
+    for (i, v) ∈ enumerate(wus)
         wu[v] = i
     end
+    wg = Dict()
 
     G = DiGraph()
-    add_vertices!(G, length(ws))
-    for o ∈ keys(ord)
+    add_vertices!(G, length(wus))
+    for o ∈ os
         sources = [ 1 ]
         for j ∈ ord[o]
             for t ∈ j.wus
                 for s ∈ sources
-                    add_edge!(G, s, wu[t])
+                    res = add_edge!(G, s, wu[t])
+                    res ? wg[s*1000000+wu[t]] = 1 : wg[s*1000000+wu[t]] += 1
                 end
             end
             sources = [wu[w] for w ∈ j.wus]
         end
         for s ∈ sources
-            add_edge!(G, s, length(ws))
+            res = add_edge!(G, s, length(wus))
+            res ? wg[s*1000000+length(wus)] = 1 : wg[s*1000000+length(wus)] += 1
         end
     end
-    (G, ws)
+    weight = [wg[src(e)*1000000+dst(e)] for e ∈ edges(G)]
+    (G, wus, weight)
 end
+
+ordergraph(ord::Orders, o::String) = ordergraph(ord, [ o ])
+ordergraph(ord::Orders) = ordergraph(ord, String[])
 
 """
     flowgraph(pr::Products)
@@ -53,4 +74,36 @@ end
 return a Digraph built from a PFlow Products array
 """
 function flowgraph(pr::Products)
+    ws = String[]              # create access to work units
+    for p ∈ pr
+        for j ∈ p.jobs
+            push!(ws, j.wu)
+        end
+    end
+    ws = collect(Set(ws))
+    sort!(ws)
+    unshift!(ws, "IN")
+    push!(ws, "OUT")
+    wuno = Dict()
+    for (i, v) ∈ enumerate(ws)
+        wuno[v] = i
+    end
+    wg = Dict()
+
+    G = DiGraph()
+    add_vertices!(G, length(ws))
+    for p ∈ pr
+        src = 1
+        for j ∈ p.jobs
+            dst = wuno[j.wu]
+            res = add_edge!(G, src, dst)
+            res ? wg[src*1000000+dst] = 1 : wg[src*1000000+dst] += 1
+            src = dst
+        end
+        dst = length(ws)
+        res = add_edge!(G, src, dst)
+        res ? wg[src*1000000+dst] = 1 : wg[src*1000000+dst] += 1
+    end
+    weight = [wg[src(e)*1000000+dst(e)] for e ∈ edges(G)]
+    (G, ws, weight)
 end
